313649218c3036187673e221ca510282c03ee6ac
Maxime <mlacassegermain@cmaisonneuve.qc.ca>
2025-11-13 08:54:27 -0500

    changement

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import eslint from "vite-plugin-eslint";
 export default defineConfig({
-    plugins: [tailwindcss(), eslint()],
+    plugins: [tailwindcss()],
 });
