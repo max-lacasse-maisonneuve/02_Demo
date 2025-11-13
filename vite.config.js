b1d1397c2af930b2d3487c3da807c601732fd395
Maxime <mlacassegermain@cmaisonneuve.qc.ca>
2025-11-13 11:07:08 -0500

    ajout rollup

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import eslint from "vite-plugin-eslint";
-import vercel from "vite-plugin-vercel";
+
 import viteImageMin from "vite-plugin-imagemin";
 
 export default defineConfig({
     plugins: [
         tailwindcss(),
-        vercel(),
         viteImageMin({
             mozjpeg: {
                 quality: 50,
